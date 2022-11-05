9e6f8541d4034ea38624e47ad63e10462b79f7c9
luka1104 <skyward.luke11@gmail.com>
2022-11-05 03:22:54 -0700

    feature/import header image

--- zabton-app/components/Header/index.tsx
+++ zabton-app/components/Header/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Center, Text } from '@chakra-ui/react'
+import { Center } from '@chakra-ui/react'
 import NextLink from 'next/link'
+import Image from 'next/image'
+import headerBanner from 'assets/images/headerBanner.png'
 
 const Header: React.FC = () => {
   return (
     <>
       <Center top='0' position='fixed' zIndex='overlay' w='100%' h='60px' bg='#F445BE' border='3px solid #242424' fontSize='2xl'>
         <NextLink href='/' passHref>
-          <Text color='white'>
-            ZABTON
-          </Text>
+          <Image src={headerBanner} alt='header banner' />
         </NextLink>
       </Center>
     </>
